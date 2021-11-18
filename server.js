2f05da3d7c6f198b3e2b29e2acf71dc281c1439f
tanookibruv <erendon1836@gmail.com>
2021-11-17 19:55:43 -0800

    adding file again

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.mongooseuri || "mongodb://localhost/budget", 
+  process.env.mongod_uri || "mongodb://localhost/budget", 
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
